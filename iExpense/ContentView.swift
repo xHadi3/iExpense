e90845eda9885461723dc149a0f3703e6939af70
xHadi3 <hadiessam9@gmail.com>
2025-01-03 16:21:40 +0300

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Hadi Al zayer on 03/07/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Hadi Al zayer on 03/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
