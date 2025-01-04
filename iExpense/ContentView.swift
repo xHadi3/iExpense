a7f2e1e24bfb1e0d9ac9fbb07c4dd694a45bb5a3
xHadi3 <hadiessam9@gmail.com>
2025-01-04 23:26:53 +0300

    .

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
+    
     var body: some View {
         NavigationStack{
             VStack {
                
         }
         }
+      
         
     }
    
