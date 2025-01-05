471cb9ff06547d32c2c878117c986bc522885486
xHadi3 <hadiessam9@gmail.com>
2025-01-05 18:49:21 +0300

    makeing the app + making it save the changes even when the app is closed

---
+++ iExpense/addView.swift
@@ -0,0 +1,18 @@
+//
+//  addView.swift
+//  iExpense
+//
+//  Created by Hadi Al zayer on 05/07/1446 AH.
+//
+
+import SwiftUI
+
+struct addView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addView()
+}
