9d55020d1cb72642a6d628acf772cb2a1858a62c
xHadi3 <hadiessam9@gmail.com>
2025-01-05 19:03:40 +0300

    fixing the sheet so it dismisses when done and adding more info to the main screan

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,19 +7,70 @@
 
 import SwiftUI
 
+struct ExpenseItem : Identifiable , Codable{
+    var id = UUID()
+    let name : String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses{
+    var items = [ExpenseItem](){
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items){
+                UserDefaults.standard.set(encoded, forKey:"Items" )
+            }
+        }
+    }
+    
+    init(){
+        if let savedItems = UserDefaults.standard.data(forKey: "Items"){
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems){
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+    
+}
 
 struct ContentView: View {
-    
+    @State private var expenses = Expenses()
+    @State private var showAddView = false
     var body: some View {
         NavigationStack{
-            VStack {
-               
+            List {
+                ForEach(expenses.items){ item in
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text(item.name).font(.headline)
+                            Text(item.type).font(.subheadline)
+                            
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
+                }
+                .onDelete(perform: removeItem)
         }
+            .navigationTitle("iExpense")
+            .toolbar{
+                Button("Add Expense", systemImage: "plus"){
+                   showAddView = true
+                }
+            }
+            .sheet(isPresented: $showAddView){
+                addView(expense: expenses)
+            }
         }
       
         
     }
-   
+    func removeItem(at offsets:IndexSet){
+        expenses.items.remove(atOffsets: offsets)
+    }
     
 }
 
--- iExpense/addView.swift
+++ iExpense/addView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct addView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Business","Personal"]
+    
+    var expense : Expenses
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            Form{
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type){
+                    ForEach(types , id: \.self){
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar{
+                Button("Svae"){
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expense.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+        
     }
+    
 }
 
 #Preview {
-    addView()
+    addView(expense: Expenses())
 }
